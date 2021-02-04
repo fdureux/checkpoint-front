ea7e3b3458f11a56988394d996da8a5bed89f1ab
fdureux <fanny.dureux@gmail.com>
2021-02-04 10:42:21 +0100

    change home for desktop

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,24 +6,33 @@ import "../styles/css/LandingPage.css";
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      {" "}
-      <h1>E-BookShelf</h1>
+      <section>
+        <h1>E-BookShelf</h1>
+        <svg
+          id="svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="-300 0 950 270">
+          <path
+            d="M-314,267 C105,364 400,100 812,279"
+            fill="none"
+            stroke="white"
+            stroke-width="120"
+            stroke-linecap="round"
+          />
+        </svg>
+        <div className="subtitle">
+          <span className="fade-1"> Lire .</span>{" "}
+          <span className="fade-2"> Partager . </span>{" "}
+          <span className="fade-3"> Découvrir .</span>
+        </div>
+        <div className="presentation">
+          {" "}
+          E-Bookshelf vous permet de gérer votre bibliothèque. Ajouter des
+          livres, découvrez-en de nouveau, ajouter des commentaires et partez
+          explorer les bibliothèques des autres!
+        </div>
+      </section>
       <img src={welcome} alt="tiny couple reading in sofa" />{" "}
-      <div className="swiper">
-        <ul class="swipe-words">
-          <li> Lire</li>
-          <li> Partager </li>
-          <li> Découvrir </li>
-        </ul>
-      </div>
-      <div className="presentation">
-        {" "}
-        E-Bookshelf vous permet de gérer votre bibliothèque. Ajouter des livres,
-        découvrez-en de nouveau et parter explorer les bibliothèques des autres!
-        Pour les amoureux des statistiques vous pourrez trouver le nombre de
-        pages lues par an, votre auteur préféré ou la nationalité de vos auteurs
-        favoris
-      </div>
       <div className="access">
         <Link to="/profile" className="hvr-rectangle-out">
           {" "}
@@ -35,6 +44,7 @@ const LandingPage = () => {
         </Link>
       </div>
     </div>
+    // </div>
   );
 };
 
